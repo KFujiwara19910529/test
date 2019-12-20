b557644194c2354bc74ee1219568ad7b43d74731
KFujiwara19910529 <kfa.k.a.kchang@gmail.com>
2019-12-20 21:42:53 +0900

    20191220

--- WebProj2/sql/testdb.sql
+++ WebProj2/sql/testdb.sql
@@ -8,13 +8,12 @@ user_name varchar(255),
 password varchar(255)
 );
 
-insert into users value(1,"taro","123");
-insert into users value(2,"jiro","456");
-insert into users value(3,"hanako","789");
+insert into users values(1,"taro","123")
+insert into users values(2,"jiro","456")
+insert into users values(3,"hanako","789")
 
 create table inquiry(
 name varchar(255),
 qtype varchar(255),
 body varchar(255)
 );
-
